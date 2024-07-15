f49a0ee31a26c4ce0aaaa0a405f217493981c576
sungsik.kim <skystar819@gmail.com>
2024-07-15 23:27:59 +0900

    캐시 적용을 위한 데코레이터 패턴 추가

--- src/main/java/tobyspring/hellospring/ObjectFactory.java
+++ src/main/java/tobyspring/hellospring/ObjectFactory.java
@@ -1,19 +1,24 @@
 package tobyspring.hellospring;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan
 public class ObjectFactory {
-//    @Bean
-//    public PaymentService paymentService() {
-//        return new PaymentService(exRateProvider());
-//    }
-//
-//    @Bean
-//    public ExRateProvider exRateProvider() {
-//        return new SimpleExRateProvider();
-//    }
+    @Bean
+    public PaymentService paymentService() {
+        return new PaymentService(cachedExRateProvider());
+    }
+
+    @Bean
+    public ExRateProvider cachedExRateProvider() {
+        return new CachedExRateProvider(exRateProvider());
+    }
+
+
+
+    @Bean
+    public ExRateProvider exRateProvider() {
+        return new WebApiExRateProvider();
+    }
 }
--- src/main/java/tobyspring/hellospring/SimpleExRateProvider.java
+++ src/main/java/tobyspring/hellospring/SimpleExRateProvider.java
@@ -1,11 +1,8 @@
 package tobyspring.hellospring;
 
-import org.springframework.stereotype.Component;
-
 import java.io.IOException;
 import java.math.BigDecimal;
 
-@Component
 public class SimpleExRateProvider implements ExRateProvider {
     @Override
     public BigDecimal getExRate(String currency) throws IOException {
