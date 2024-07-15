668b85ddcd86596f23418df8c5ede494655c0a5b
sungsik.kim <skystar819@gmail.com>
2024-07-16 07:23:52 +0900

    AnnotationConfigApplicationContext 사용하여 테스트 코드 수정

---
+++ src/test/java/tobyspring/hellospring/TestObjectFactory.java
@@ -0,0 +1,29 @@
+package tobyspring.hellospring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import tobyspring.hellospring.exrate.CachedExRateProvider;
+import tobyspring.hellospring.exrate.WebApiExRateProvider;
+import tobyspring.hellospring.payment.ExRateProvider;
+import tobyspring.hellospring.payment.ExRateProviderStub;
+import tobyspring.hellospring.payment.PaymentService;
+
+import java.math.BigDecimal;
+
+@Configuration
+public class TestObjectFactory {
+    @Bean
+    public PaymentService paymentService() {
+        return new PaymentService(cachedExRateProvider());
+    }
+
+    @Bean
+    public ExRateProvider cachedExRateProvider() {
+        return new CachedExRateProvider(exRateProvider());
+    }
+
+    @Bean
+    public ExRateProvider exRateProvider() {
+        return new ExRateProviderStub(BigDecimal.valueOf(1000));
+    }
+}
---
+++ src/test/java/tobyspring/hellospring/payment/PaymentServiceSpringTest.java
@@ -0,0 +1,28 @@
+package tobyspring.hellospring.payment;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import tobyspring.hellospring.ObjectFactory;
+import tobyspring.hellospring.TestObjectFactory;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PaymentServiceSpringTest {
+
+    @Test
+    @DisplayName("prepare 메소드가 요구사항 3가지를 잘 충족했는지 검증")
+    void convertedAmount() throws IOException {
+        BeanFactory beanFactory = new AnnotationConfigApplicationContext(TestObjectFactory.class);
+        PaymentService paymentService = beanFactory.getBean(PaymentService.class);
+
+        Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
+
+        assertThat(payment.getExRate()).isEqualByComparingTo(BigDecimal.valueOf(1_000));
+        assertThat(payment.getConvertedAmount()).isEqualByComparingTo(BigDecimal.valueOf(10_000));
+    }
+}
