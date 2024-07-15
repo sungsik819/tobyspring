95db418a5816773a9b8762bd4d8d2dd6701210ad
sungsik.kim <skystar819@gmail.com>
2024-07-16 07:27:51 +0900

    annotation 사용하여 주입

--- src/test/java/tobyspring/hellospring/payment/PaymentServiceSpringTest.java
+++ src/test/java/tobyspring/hellospring/payment/PaymentServiceSpringTest.java
@@ -2,8 +2,12 @@ package tobyspring.hellospring.payment;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import tobyspring.hellospring.ObjectFactory;
 import tobyspring.hellospring.TestObjectFactory;
 
@@ -12,12 +16,14 @@ import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = TestObjectFactory.class)
 class PaymentServiceSpringTest {
+    @Autowired BeanFactory beanFactory;
 
     @Test
     @DisplayName("prepare 메소드가 요구사항 3가지를 잘 충족했는지 검증")
     void convertedAmount() throws IOException {
-        BeanFactory beanFactory = new AnnotationConfigApplicationContext(TestObjectFactory.class);
         PaymentService paymentService = beanFactory.getBean(PaymentService.class);
 
         Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
