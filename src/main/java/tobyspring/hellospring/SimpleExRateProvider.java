c8dbc8a958df9ad719169ed86da9e536d818b3a9
sungsik.kim <skystar819@gmail.com>
2024-07-15 18:01:58 +0900

    클래스로 분리

--- src/main/java/tobyspring/hellospring/SimpleExRateProvider.java
+++ src/main/java/tobyspring/hellospring/SimpleExRateProvider.java
@@ -3,9 +3,8 @@ package tobyspring.hellospring;
 import java.io.IOException;
 import java.math.BigDecimal;
 
-public class SimpleExRatePaymentService extends PaymentService {
-    @Override
-    BigDecimal getExRate(String currency) throws IOException {
+public class SimpleExRateProvider {
+    public BigDecimal getExRate(String currency) throws IOException {
         if(currency.equals("USD")) return BigDecimal.valueOf(1000);
 
         throw new IllegalArgumentException("지원되지 않는 통화입니다.");
