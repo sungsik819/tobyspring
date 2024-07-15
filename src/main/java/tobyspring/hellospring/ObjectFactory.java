6f4b5377440a0f6cf22856b5ccc857d97576af36
sungsik.kim <skystar819@gmail.com>
2024-07-15 19:27:57 +0900

    component를 이용한 DI 방법

--- src/main/java/tobyspring/hellospring/ObjectFactory.java
+++ src/main/java/tobyspring/hellospring/ObjectFactory.java
@@ -1,17 +1,19 @@
 package tobyspring.hellospring;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan
 public class ObjectFactory {
-    @Bean
-    public PaymentService paymentService() {
-        return new PaymentService(exRateProvider());
-    }
-
-    @Bean
-    public ExRateProvider exRateProvider() {
-        return new SimpleExRateProvider();
-    }
+//    @Bean
+//    public PaymentService paymentService() {
+//        return new PaymentService(exRateProvider());
+//    }
+//
+//    @Bean
+//    public ExRateProvider exRateProvider() {
+//        return new SimpleExRateProvider();
+//    }
 }
