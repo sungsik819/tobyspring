e42a23d6d0b2e1bbef208214537c254ac4f9c742
sungsik.kim <skystar819@gmail.com>
2024-07-15 18:10:51 +0900

    인터페이스로 변경

--- src/main/java/tobyspring/hellospring/SimpleExRateProvider.java
+++ src/main/java/tobyspring/hellospring/SimpleExRateProvider.java
@@ -3,7 +3,8 @@ package tobyspring.hellospring;
 import java.io.IOException;
 import java.math.BigDecimal;
 
-public class SimpleExRateProvider {
+public class SimpleExRateProvider implements ExRateProvider {
+    @Override
     public BigDecimal getExRate(String currency) throws IOException {
         if(currency.equals("USD")) return BigDecimal.valueOf(1000);
 
--- src/main/java/tobyspring/hellospring/WebApiExRateProvider.java
+++ src/main/java/tobyspring/hellospring/WebApiExRateProvider.java
@@ -10,8 +10,10 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.stream.Collectors;
 
-public class WebApiExRateProvider {
-    public BigDecimal getWebExRate(String currency) throws IOException  {
+public class WebApiExRateProvider implements ExRateProvider {
+
+    @Override
+    public BigDecimal getExRate(String currency) throws IOException  {
         URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
