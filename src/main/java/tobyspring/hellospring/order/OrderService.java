b79db062c7dc7b131f203805d696a53946b80533
sungsik.kim <skystar819@gmail.com>
2024-07-16 16:18:44 +0900

    JpaOrderRepository를 OrderRepository로 추상화 OrderService 테스트 코드 작성

--- src/main/java/tobyspring/hellospring/order/OrderService.java
+++ src/main/java/tobyspring/hellospring/order/OrderService.java
@@ -3,7 +3,6 @@ package tobyspring.hellospring.order;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.support.TransactionTemplate;
-import tobyspring.hellospring.data.OrderRepository;
 
 import java.math.BigDecimal;
 
