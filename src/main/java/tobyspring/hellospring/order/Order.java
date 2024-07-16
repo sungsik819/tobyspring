e9cb498d32d3014f06b4df1d243719e1a1d1b206
sungsik.kim <skystar819@gmail.com>
2024-07-16 16:07:50 +0900

    jpa annotation 사용하지 않고 xml로 변경

--- src/main/java/tobyspring/hellospring/order/Order.java
+++ src/main/java/tobyspring/hellospring/order/Order.java
@@ -4,13 +4,9 @@ import jakarta.persistence.*;
 
 import java.math.BigDecimal;
 
-@Entity
-@Table(name = "orders")
 public class Order {
-    @Id @GeneratedValue
     private Long id;
 
-    @Column(unique = true)
     private String no;
 
     private BigDecimal total;
