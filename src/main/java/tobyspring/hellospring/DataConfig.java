ae6e69cc77ef683c4034d6b2df48295d5cafe3d0
sungsik.kim <skystar819@gmail.com>
2024-07-16 15:09:23 +0900

    Data 예외 추상화 적용

--- src/main/java/tobyspring/hellospring/DataConfig.java
+++ src/main/java/tobyspring/hellospring/DataConfig.java
@@ -1,15 +1,17 @@
 package tobyspring.hellospring;
 
 import jakarta.persistence.EntityManagerFactory;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.support.PersistenceAnnotationBeanPostProcessor;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import tobyspring.hellospring.data.OrderRepository;
-import tobyspring.hellospring.order.Order;
 
 import javax.sql.DataSource;
 
@@ -37,7 +39,17 @@ public class DataConfig {
     }
 
     @Bean
-    public OrderRepository orderRepository(EntityManagerFactory emf) {
-        return new OrderRepository(emf);
+    public BeanPostProcessor persistenceAnnotationBeanPostProcessor() {
+        return new PersistenceAnnotationBeanPostProcessor();
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
+        return new JpaTransactionManager(emf);
+    }
+
+    @Bean
+    public OrderRepository orderRepository() {
+        return new OrderRepository();
     }
 }
