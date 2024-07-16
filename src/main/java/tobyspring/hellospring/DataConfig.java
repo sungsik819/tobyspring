1eda37dcd3cee5a494ee38047ac08f2c055d3ba1
sungsik.kim <skystar819@gmail.com>
2024-07-16 15:46:03 +0900

    애플리케이션 서비스 도입을 위한 OrderService 추가

--- src/main/java/tobyspring/hellospring/DataConfig.java
+++ src/main/java/tobyspring/hellospring/DataConfig.java
@@ -47,9 +47,4 @@ public class DataConfig {
     public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
         return new JpaTransactionManager(emf);
     }
-
-    @Bean
-    public OrderRepository orderRepository() {
-        return new OrderRepository();
-    }
 }
